0dba1a8a849c76a9efb32af7b349c42e716ddb1f
bartgerard <bart.gerard@gmail.com>
2024-12-01 06:21:31 +0100

    :sparkles: year 2024 day 1

--- src/main/kotlin/aock2024/Year2024Day01.kt
+++ src/main/kotlin/aock2024/Year2024Day01.kt
@@ -1,8 +1,31 @@
 package aock2024
 
+import shared.asLongs
+import shared.byLine
+import kotlin.math.abs
+
 data class Year2024Day01(
-    private val text: String
+    private val pairs: List<Pair<Long, Long>>
 ) {
-    fun partOne(): Int = 0
-    fun partTwo(): Int = 0
+    constructor(text: String) : this(
+        text.byLine()
+            .map { it.asLongs() }
+            .map { Pair(it[0], it[1]) }
+    )
+
+    fun distanceBetweenLists(): Long {
+        val firstList = pairs.map { it.first }.sorted()
+        val secondList = pairs.map { it.second }.sorted()
+
+        return firstList.indices.sumOf { abs(firstList[it] - secondList[it]) }
+    }
+
+    fun similarityScore(): Long {
+        val firstList = pairs.map { it.first }.sorted()
+        val frequenciesByNumber: Map<Long, Int> = pairs.map { it.second }
+            .groupBy { it }
+            .mapValues { it.value.size }
+
+        return firstList.indices.sumOf { firstList[it] * (frequenciesByNumber[firstList[it]] ?: 0) }
+    }
 }
--- src/test/groovy/aock2024/Year2024Day01Specification.groovy
+++ src/test/groovy/aock2024/Year2024Day01Specification.groovy
@@ -6,32 +6,32 @@ import static shared.InputsKt.readFile
 
 class Year2024Day01Specification extends Specification {
 
-    def "partOne"() {
+    def "what is the total distance between your lists"() {
         when:
-        final long result = new Year2024Day01(input).partOne()
+        final long result = new Year2024Day01(input).distanceBetweenLists()
 
         then:
         result == expectedResult
 
         where:
-        input             | expectedResult | comment
-        ""                | 0              | ""
+        input                          | expectedResult | comment
+        readFile(2024, 1, "example_1") | 11             | ""
 
-        readFile(2024, 1) | 0              | ""
+        readFile(2024, 1)              | 1603498        | ""
     }
 
-    def "partTwo"() {
+    def "what is their similarity score"() {
         when:
-        final long result = new Year2024Day01(input).partTwo()
+        final long result = new Year2024Day01(input).similarityScore()
 
         then:
         result == expectedResult
 
         where:
-        input             | expectedResult | comment
-        ""                | 0              | ""
+        input                          | expectedResult | comment
+        readFile(2024, 1, "example_1") | 31             | ""
 
-        readFile(2024, 1) | 0              | ""
+        readFile(2024, 1)              | 25574739       | ""
     }
 
 }
