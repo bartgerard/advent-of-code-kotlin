fa0a8f98bc3ed46f8d527c5497a3377018e38b2c
bartgerard <bart.gerard@gmail.com>
2024-12-20 07:20:34 +0100

    :sparkles: year 2024 day 20 (part 2 draft)

--- src/main/kotlin/aock2024/Year2024Day20.kt
+++ src/main/kotlin/aock2024/Year2024Day20.kt
@@ -2,6 +2,8 @@ package aock2024
 
 import shared.CharGrid
 import shared.Dijkstra
+import shared.Point2d
+import shared.Solution
 import shared.Vector2d.Companion.ORTHOGONAL_ADJACENT
 
 data class Year2024Day20(
@@ -15,38 +17,31 @@ data class Year2024Day20(
 
     constructor(input: String) : this(CharGrid(input))
 
-    fun partOne(minimumSaving: Int): Int {
-        val start = grid.findAll(START).first()
-        val end = grid.findAll(END).first()
-
-        val solution = Dijkstra.findShortestPath(
-            start,
-            { point -> point == end },
-            { _, point ->
-                point.neighbours(ORTHOGONAL_ADJACENT)
-                    .filter { grid.contains(it) && grid.at(it) != WALL }
-            }
-        )
+    fun partOne(minimumSaving: Int, allowedShortcut: Int = 2): Int {
+        val solution = findShortestPath()
 
         val shortestPath = solution.fullPath()
         shortestPath.forEach { grid.set(it, 'O') }
 
         var cheats = 0L
-        val shortCuts = mutableListOf<Long>()
+        val shortCuts = mutableListOf<Int>()
 
-        for (step in shortestPath) {
-            val cost = shortestPath.indexOf(step)
+        for (i in shortestPath.indices) {
+            val currentStep = shortestPath[i]
 
-            shortCuts += step.neighbours(ORTHOGONAL_ADJACENT)
-                .filter { grid.contains(it) && grid.at(it) == WALL }
-                .flatMap { it.neighbours(ORTHOGONAL_ADJACENT) }
-                .filter {
-                    grid.contains(it)
-                            && grid.at(it) != WALL
-                            && it != step
+            for (j in shortestPath.indices.reversed()) {
+                val difference = j - i
+                if (difference <= minimumSaving + 2) {
+                    break
                 }
-                .map { solution.path.costTo(it) - cost - 2 } // going through the wall
-                .filter { it > 0 }
+
+                val nextStep = shortestPath[j]
+
+                val lengthOfShortcut = currentStep.manhattan(nextStep)
+                if (lengthOfShortcut in 2..allowedShortcut) {
+                    shortCuts.add(difference - lengthOfShortcut)
+                }
+            }
 
             cheats = cheats + shortCuts.size
         }
@@ -57,6 +52,62 @@ data class Year2024Day20(
         return shortCuts.filter { it >= minimumSaving }.size
     }
 
-    fun partTwo() = 0L
+    fun partTwo(minimumSaving: Int): Long {
+        val solution = findShortestPath()
+
+        val shortestPath = solution.fullPath()
+        shortestPath.forEach { grid.set(it, 'O') }
+
+        var cheats = 0L
+        val shortCuts = mutableListOf<Long>()
+
+        for (i in shortestPath.indices) {
+            val currentStep = shortestPath[i]
+            val cost = i
+
+            for (j in shortestPath.indices.reversed()) {
+                if (i + minimumSaving > j) {
+                    break
+                }
+
+                val nextStep = shortestPath[i]
+
+                if (currentStep.manhattan(nextStep) <= 20) {
+
+                }
+            }
+
+            shortCuts += currentStep.neighbours(ORTHOGONAL_ADJACENT)
+                .filter { grid.contains(it) && grid.at(it) == WALL }
+                .flatMap { it.neighbours(ORTHOGONAL_ADJACENT) }
+                .filter {
+                    grid.contains(it)
+                            && grid.at(it) != WALL
+                            && it != currentStep
+                }
+                .map { solution.path.costTo(it) - cost - 2 } // going through the wall
+                .filter { it > 0 }
+
+            cheats = cheats + shortCuts.size
+        }
+
+        val frequencies = shortCuts.groupingBy { it }.eachCount()
+        println(frequencies)
+
+        return 0
+        //return shortCuts.filter { it >= minimumSaving }.size
+    }
+
+    private fun findShortestPath(): Solution<Point2d> {
+        val start = grid.findAll(START).first()
+        val end = grid.findAll(END).first()
+
+        val solution = Dijkstra.findShortestPath(
+            start,
+            { point -> point == end },
+            { _, point -> point.neighbours(ORTHOGONAL_ADJACENT).filter { grid.contains(it) && grid.at(it) != WALL } }
+        )
+        return solution
+    }
 
 }
--- src/test/groovy/aock2024/Year2024Day20Specification.groovy
+++ src/test/groovy/aock2024/Year2024Day20Specification.groovy
@@ -9,7 +9,7 @@ class Year2024Day20Specification extends Specification {
 
     def "partOne"() {
         when:
-        final long result = new Year2024Day20(input).partOne(minimumSaving)
+        final long result = new Year2024Day20(input).partOne(minimumSaving, 2)
 
         then:
         result == expectedResult
@@ -18,21 +18,21 @@ class Year2024Day20Specification extends Specification {
         input                           | minimumSaving | expectedResult | comment
         readFile(2024, 20, "example_1") | 0             | 44             | ""
 
-        readFile(2024, 20)              | 100           | 0              | "not 6963"
+        readFile(2024, 20)              | 100           | 1310           | ""
     }
 
     def "partTwo"() {
         when:
-        final long result = new Year2024Day20(input).partTwo()
+        final long result = new Year2024Day20(input).partOne(minimumSaving, 50)
 
         then:
         result == expectedResult
 
         where:
-        input              | expectedResult | comment
-        ""                 | 0              | ""
+        input                           | minimumSaving | expectedResult | comment
+        readFile(2024, 20, "example_1") | 50            | 285            | ""
 
-        readFile(2024, 20) | 0              | ""
+        readFile(2024, 20)              | 100           | 0              | "not 7704580"
     }
 
 }
