a8a2195c664794dd646d0d7274cd87d0b8a65766
bartgerard <bart.gerard@gmail.com>
2024-12-20 08:08:31 +0100

    :sparkles: year 2024 day 20 (part 2 draft)

--- src/test/groovy/aock2024/Year2024Day20Specification.groovy
+++ src/test/groovy/aock2024/Year2024Day20Specification.groovy
@@ -23,7 +23,7 @@ class Year2024Day20Specification extends Specification {
 
     def "partTwo"() {
         when:
-        final long result = new Year2024Day20(input).partOne(minimumSaving, 50)
+        final long result = new Year2024Day20(input).partOne(minimumSaving, 20)
 
         then:
         result == expectedResult
@@ -32,7 +32,7 @@ class Year2024Day20Specification extends Specification {
         input                           | minimumSaving | expectedResult | comment
         readFile(2024, 20, "example_1") | 50            | 285            | ""
 
-        readFile(2024, 20)              | 100           | 0              | "not 7704580"
+        readFile(2024, 20)              | 100           | 0              | "not 7704580, 982858"
     }
 
 }
