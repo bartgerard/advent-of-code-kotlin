8fc93d140850ae9133a65e3a8adb3631efc5026e
bart.gerard <bart.gerard@gmail.com>
2025-10-25 16:07:52 +0200

    :sparkles: year 2021 day 2

--- src/main/kotlin/aock2021/Year2021Day02.kt
+++ src/main/kotlin/aock2021/Year2021Day02.kt
@@ -1,12 +1,40 @@
 package aock2021
 
+import shared.Point2d
+import shared.Vector2d
 import shared.sanitize
+import kotlin.math.absoluteValue
 
 data class Year2021Day02(
-    private val input: List<String>
+    private val input: List<Vector2d>
 ) {
-    constructor(input: String) : this(input.sanitize().lines())
+    companion object {
+        fun toVector(direction: String, distance: Int) = when (direction) {
+            "forward" -> Vector2d(distance, 0)
+            "up" -> Vector2d(0, -distance)
+            "down" -> Vector2d(0, distance)
+            else -> error("invalid direction $direction")
+        }
+    }
 
-    fun partOne() = 0L
-    fun partTwo() = 0L
+    constructor(input: String) : this(
+        input.sanitize().lines()
+            .map { it.split(" ") }
+            .map { it[0] to it[1].toInt() }
+            .map { (direction, distance) -> toVector(direction, distance) }
+    )
+
+    fun partOne() = input
+        .fold(Point2d.ZERO) { point, vector -> point + vector }
+        .let { (x, y) -> x.absoluteValue * y.absoluteValue }
+
+    fun partTwo() = input
+        .fold(Point2d.ZERO to 0) { (point, aim), vector ->
+            when {
+                vector.y == 0 -> point + Vector2d(vector.x, vector.x * aim) to aim
+                else -> point to (aim + vector.y)
+            }
+        }
+        .let { (point, _) -> point }
+        .let { (x, y) -> x.absoluteValue * y.absoluteValue }
 }
---
+++ src/test/groovy/aock2021/Year2021Day02Specification.groovy
@@ -0,0 +1,38 @@
+package aock2021
+
+
+import spock.lang.Specification
+
+import static shared.InputsKt.readFile
+
+class Year2021Day02Specification extends Specification {
+
+    def "partOne"() {
+        when:
+        final long result = new Year2021Day02(input).partOne()
+
+        then:
+        result == expectedResult
+
+        where:
+        input                          | expectedResult | comment
+        readFile(2021, 2, "example_1") | 150            | ""
+
+        readFile(2021, 2)              | 1654760        | ""
+    }
+
+    def "partTwo"() {
+        when:
+        final long result = new Year2021Day02(input).partTwo()
+
+        then:
+        result == expectedResult
+
+        where:
+        input                          | expectedResult | comment
+        readFile(2021, 2, "example_1") | 900            | ""
+
+        readFile(2021, 2)              | 1956047400     | ""
+    }
+
+}
