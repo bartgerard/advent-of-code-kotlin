6c01c26cac4367d2db885b2ed52c2a943db10108
bart.gerard <bart.gerard@gmail.com>
2025-10-26 09:31:53 +0100

    :sparkles: year 2016 day 2

--- src/main/kotlin/aock2016/Year2016Day02.kt
+++ src/main/kotlin/aock2016/Year2016Day02.kt
@@ -1,15 +1,46 @@
 package aock2016
 
+import shared.CharGrid
 import shared.Direction
+import shared.Point2d
+import shared.at
 import shared.sanitize
 
 data class Year2016Day02(
     private val input: List<List<Direction>>
 ) {
+    companion object {
+        val NUMERIC_KEYPAD = CharGrid("123\n456\n789")
+        val SPECIAL_KEYPAD = CharGrid("  1  \n 234 \n56789\n ABC \n  D  ")
+    }
+
     constructor(input: String) : this(
         input.sanitize().lines()
             .map { line -> line.map { Direction.parse(it) } })
 
-    fun partOne() = 0L
-    fun partTwo() = 0L
+    fun partOne() = traverseKeyPad(
+        NUMERIC_KEYPAD.findAll('5').first(),
+        NUMERIC_KEYPAD
+    )
+
+    fun partTwo() = traverseKeyPad(
+        SPECIAL_KEYPAD.findAll('5').first(),
+        SPECIAL_KEYPAD
+    )
+
+    private fun traverseKeyPad(
+        start: Point2d,
+        keypad: CharGrid
+    ): String = input.scan(start) { point, directions ->
+        directions.fold(point) { oldPoint, direction ->
+            val newPoint = oldPoint + direction
+            if (keypad.contains(newPoint) && keypad.at(newPoint) != ' ') {
+                newPoint
+            } else {
+                oldPoint
+            }
+        }
+    }
+        .drop(1)
+        .joinToString("") { keypad.at(it).toString() }
 }
--- src/test/groovy/aock2016/Year2016Day02Specification.groovy
+++ src/test/groovy/aock2016/Year2016Day02Specification.groovy
@@ -9,31 +9,31 @@ class Year2016Day02Specification extends Specification {
 
     def "partOne"() {
         when:
-        final long result = new Year2016Day02(input).partOne()
+        final String result = new Year2016Day02(input).partOne()
 
         then:
         result == expectedResult
 
         where:
         input                          | expectedResult | comment
-        readFile(2016, 2, "example_1") | 1985           | ""
+        readFile(2016, 2, "example_1") | "1985"         | ""
 
 
-        readFile(2016, 2)              | 0              | ""
+        readFile(2016, 2)              | "95549"        | ""
     }
 
     def "partTwo"() {
         when:
-        final long result = new Year2016Day02(input).partTwo()
+        final String result = new Year2016Day02(input).partTwo()
 
         then:
         result == expectedResult
 
         where:
-        input             | expectedResult | comment
-        ""                | 0              | ""
+        input                          | expectedResult | comment
+        readFile(2016, 2, "example_1") | "5DB3"         | ""
 
-        readFile(2016, 2) | 0              | ""
+        readFile(2016, 2)              | "D87AD"        | ""
     }
 
 }
